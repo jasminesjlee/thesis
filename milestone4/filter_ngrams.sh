f2da613c01a4cfc3614357baefc7a836f6f1e889
Sun Jae Lee <leesunj@NLPGRID.seas.upenn.edu>
2019-08-10 20:35:03 -0400

    fixed filter ngrams script

--- milestone4/filter_ngrams.sh
+++ milestone4/filter_ngrams.sh
@@ -24,20 +24,22 @@ declare -a write_files=(
 "ngrams_5.txt"
 )
 
+
 for idx in "${!ngram_folders[@]}"
 do
+    echo "LOADING FROM NGRAMS $idx"
     ngram_folder=${ngram_folders[$idx]}
     template_file=${template_files[$idx]}
-    word_file=${word_files[$idx]}
     write_file=${write_files[$idx]}
-    while IFS='	' read -r t1
+    for word_file in ${word_files[@]}
     do
-        while IFS='	' read -r w1 w2 w3
+	echo "file : $word_file"
+        while IFS='	' read -r t1
         do
-            #for f in $ngram_folder
-            #do
-            zcat $ngram_folder/* | grep -E "$t1" >> $write_file
-            #done
-        done <$word_file
-    done <$template_file
+            while IFS='	' read -r w1 w2 w3
+            do
+                zcat $ngram_folder/* | grep -E "$t1" >> $write_file
+            done <$word_file
+        done <$template_file
+    done
 done
