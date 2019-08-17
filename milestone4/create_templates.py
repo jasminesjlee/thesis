863687c45adac7d844f6f2885c3158a499a4c038
Jasmine Lee <leezsun@amazon.com>
2019-08-17 02:40:12 -0400

    add script

---
+++ milestone4/create_templates.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+if __name__=='__main__':
+    with open("templates.txt") as f:
+        out3 = open("template_grep_3.txt", 'w')
+        out4 = open("template_grep_4.txt", 'w')
+        out5 = open("template_grep_5.txt", 'w')
+        lemmas = open("all_lemmas.txt", 'w')
+        out_new = open("new_templates.txt", 'w')
+        for f in [out3, out4, out5]:
+            for line in f:
+                for lemma_line in lemmas:
+                    w1, w2, w3, _ = lemma_line.split('\t')
+                    new_line = line.replace("${w2}", w2).replace("${w1}", w1)
+                    out_new.write(new_line)
+
+                    new_line = line.replace("${w2}", w3).replace("${w1}", w2)
+                    out_new.write(new_line)
